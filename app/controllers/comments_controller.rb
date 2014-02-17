e45cad807ab89451f69314a1f66ef4257a910dcb
Brad Lowenstein <blowenstein93@gmail.com>
2014-02-16 22:29:19 -0500

    setup links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,29 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
   end
 
   def new
+  	@comment = Comment.new
   end
 
+  def create
+  	@comment.Comment.new(comment_params)
+  	if @comment.save
+  		redirect_to comment_path
+  	else
+  		render 'new'
+  	end
+  end
+  	
   def show
   end
 
   def edit
+  end
+
+  private
+  def comment_params
+  	params.require(:user, :comment)
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,19 @@
 class VideosController < ApplicationController
   def index
+    @videos = Video.all
   end
 
   def new
+  	@video = Video.new
+  end
+
+  def create
+  	@video.Video.new(video_params)
+  	if @video.save
+  		redirect_to video_path
+  	else 
+  		render 'new'
+  	end
   end
 
   def show
@@ -10,4 +21,10 @@ class VideosController < ApplicationController
 
   def edit
   end
+
+  private 
+  def user_params
+  	params.require(:file, :user)
+  end
+
 end
