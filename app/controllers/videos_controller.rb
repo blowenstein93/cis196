eb629686bfafe4141e62ead3ef789d5464b04149
Brad Lowenstein <blowenstein93@gmail.com>
2014-02-21 18:05:28 -0500

    hw5 added edit, show, delete

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,9 +17,26 @@ class VideosController < ApplicationController
   end
 
   def show
+    @video = Video.find(params[:id])
   end
 
   def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(video_params)
+      redirect_to video_path(@video.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to videos_path
   end
 
   private 
