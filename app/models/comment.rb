bc192fa18fde064c4d26eb3d27074a0b947866c9
Brad Lowenstein <blowenstein93@gmail.com>
2014-03-07 14:02:16 -0500

    hw7, added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
+
+  validates :comment, length: {within: 50 ... 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+   validates :account_name, uniqueness: true
+   validates_inclusion_of :age, :in => 18...100 , :message => 'Must be between 18 and 100 years old to join... Sorry Grandma!'
+
 end
