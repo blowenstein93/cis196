8c8c8a77192cba56defb4cb8c76b289b5767981b
Brad Lowenstein <blowenstein93@gmail.com>
2014-04-21 23:35:43 -0400

    hw10 fixed javascript function wrapper

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,12 +1,11 @@
 $(document).on("page:load", function() {
-		$("#toggle_comments").click(function() {
-			if( $(".vid_comment").hasClass("hide")) {
-				$(".vid_comment").removeClass("hide");
-				$("#toggle_comments").html("Hide Comments");
-			}else{
-				$(".vid_comment").addClass("hide");
-				$("#toggle_comments").html("Show Comments");
-			}
-		});
+	$("#toggle_comments").click(function() {
+		if( $(".vid_comment").hasClass("hide")) {
+			$(".vid_comment").removeClass("hide");
+			$("#toggle_comments").html("Hide Comments");
+		}else{
+			$(".vid_comment").addClass("hide");
+			$("#toggle_comments").html("Show Comments");
+		}
 	});
 });
